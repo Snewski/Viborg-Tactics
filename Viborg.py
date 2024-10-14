9527b4057731d581d1ba8b4f48b1b586dc6660a2
Snewski <peterhw@hotmail.dk>
2024-10-14 22:19:54 +0200

    Made to-do list

--- Viborg.py
+++ Viborg.py
@@ -1,28 +1,40 @@
 ### Viborg-Tactics ###
 
+
+## To-Do ##
+
+# Write text chunks for the different parts (consent, instructions, etc.)
+# Make windows to show stimuli
+# Find video stimuli and group with red dots
+# Make pictures for tactical decisions and make sure data is collected in the desired format
+# Make 2-3 practice trials to familiarize participants
+
+
 ## Importing modules ##
 from psychopy import visual, event, core, gui, data, clock
 import pandas as pd, random, os
+
 
 ## Logfile ##
 # Making sure there is a logfile directory
 if not os.path.exists("logfiles"):
     os.makedirs("logfiles")
 
-list_of_numbers = list(range(1, 17)) # Options for years of experience
 
 ## GUI ##
 # Creating dialogue box
 DialogueBox = gui.Dlg(title = "Viborg Tactics")
 DialogueBox.addText('Udfyld venligst de nedenstående felter:')
+list_of_numbers = list(range(1, 17)) # Options for years of experience
 # Adding information fields
 DialogueBox.addField('Position:', choices = ['Fosvarsspiller', 'Midtbanespiller', 'Angriber'], color = "green")
-DialogueBox.addField('Trøjenummer:', color = "black")
+DialogueBox.addField('Dit trøjenummer:', color = "black")
 DialogueBox.addField('Hvilken fod er din dominante?:', choices = ['Højre', 'Venstre'],color = "green")
 DialogueBox.addField('Alder:', color = "black")
 DialogueBox.addField('Hvor mange år har du spillet fodboldt?:', choices = list_of_numbers, color = "green")
 # Showing dialogue box
 DialogueBox.show()
+
 
 # Collecting participant information
 if DialogueBox.OK:
@@ -34,11 +46,25 @@ if DialogueBox.OK:
 else:
     core.quit()
 
-# Setting up dataframe
+
+## Setting up dataframe ##
 columns = ['Number', 'Position', 'Age', 'Foot', 'Experience'] # Need columns for the tactical data
 logfile = pd.DataFrame(columns = columns)
 
 
+## All text chunks ##
+
+
+
+
+## Presenting introduction/consent ##
+win = visual.Window(fullscr = True)
+instruction = visual.TextStim(win,text = ?, color="black", height=0.08) # needs consent form/introduction
+instruction.draw()
+win.flip()
+event.waitKeys()
+# Close the window
+win.close()
 
 
 
@@ -51,8 +77,6 @@ logfile = pd.DataFrame(columns = columns)
 
 
 
-
-
-# Save logfile
+## Save logfile ##
 logfile_name = f"logfiles/logfile_{Number}.csv"
 logfile.to_csv(logfile_name)
